ed512ea73697f2d794b0c4a745fcbd7c2efcab0e
shima <shymaqyaswnd@gmail.com>
2025-10-15 17:51:36 +0330

    first commit

---
+++ weblog/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace weblog.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            ViewBag.BlogId = id;
+            return View();
+        }
+        public IActionResult Archive()
+        {
+            return View();
+        }
+    }
+}
